39cc82872eba441ad72aa6684c1cfe0e5c3f3e67
João Rodolfo Reiter <joaorodolforeiter@protonmail.com>
2023-02-17 20:01:11 -0300

    changed shopping cart icon

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,3 +1,4 @@
+import { ShoppingCart } from "phosphor-react";
 import { Outlet, Link } from "react-router-dom";
 
 export default function Layout() {
@@ -10,13 +11,9 @@ export default function Layout() {
         </div>
         <div className="flex items-center justify-center gap-3">
           <a href="./carrinho.html">
-            <img
-              className="w-10"
-              src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Femojigraph.org%2Fmedia%2Fmicrosoft%2Fshopping-cart_1f6d2.png&f=1&nofb=1&ipt=15b563227774bf490ae6f11eb4efb142f9deb0d099f8b6b5c905b12363a6ffa2&ipo=images"
-              alt=""
-            />
+            <ShoppingCart size={32} weight={"fill"} />
           </a>
-          <div className="text-lg">Pet JavaBot</div>
+          <div className="text-lg font-bold">Pet JavaBot</div>
           <img
             className="h-12"
             src="https://imagensemoldes.com.br/wp-content/uploads/2021/03/Ping-Pong-PNG.png"
