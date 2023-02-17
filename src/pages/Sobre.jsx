ac257e429cfefb6ed094ec3b27b8d01e12936179
João Rodolfo Reiter <joaorodolforeiter@protonmail.com>
2023-02-17 20:20:51 -0300

    fixed about page

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,23 +1,18 @@
 export default function Sobre() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-zinc-900">
-      <div className="relative flex w-full flex-1 flex-col items-center justify-center gap-12">
-        <h1 className="text-center text-4xl text-emerald-300">
-          Sobre o Pet JavaBot
-        </h1>
-        <p className="max-w-3xl p-6 text-justify text-lg text-emerald-100">
-          O Pet JavaBot é um robô de estimação virtual programado em Java. Ele é
-          capaz de interagir com seus usuários e realizar várias tarefas, desde
-          jogar jogos até lembrar de compromissos. Nossa equipe de
-          desenvolvimento está empenhada em fornecer a melhor experiência
-          possível para os nossos usuários. O Pet JavaBot é um projeto de código
-          aberto e estamos sempre abertos a contribuições da comunidade. Também
-          e possível você adquirir um pet na nossa loja que está em constante
-          mudança na variação.
-        </p>
-      </div>
-
-      <script src="/index.js"></script>
+    <div className="flex flex-1 flex-col items-center justify-center gap-12 bg-zinc-900">
+      <h1 className="text-center text-4xl text-emerald-300">
+        Sobre o Pet JavaBot
+      </h1>
+      <p className="max-w-3xl p-6 text-justify text-lg text-emerald-100">
+        O Pet JavaBot é um robô de estimação virtual programado em Java. Ele é
+        capaz de interagir com seus usuários e realizar várias tarefas, desde
+        jogar jogos até lembrar de compromissos. Nossa equipe de desenvolvimento
+        está empenhada em fornecer a melhor experiência possível para os nossos
+        usuários. O Pet JavaBot é um projeto de código aberto e estamos sempre
+        abertos a contribuições da comunidade. Também e possível você adquirir
+        um pet na nossa loja que está em constante mudança na variação.
+      </p>
     </div>
   );
 }
