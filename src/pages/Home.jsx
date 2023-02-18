c0270f94f4dde82d2bf2f37f193016a0beedb47d
João Rodolfo Reiter <joaorodolforeiter@protonmail.com>
2023-02-17 21:49:39 -0300

    updated homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,58 +1,34 @@
-import { useState } from "react";
+import { Link } from "react-router-dom";
+import { animals } from "../animals";
 
 export default function Home() {
-  const [visible, setVisible] = useState(false);
   return (
-    <div className="flex w-full flex-1 flex-col items-center justify-center gap-12 bg-zinc-900">
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-        className="rounded-xl border-2 border-emerald-400 shadow-md shadow-emerald-100 transition-all hover:scale-110 hover:shadow-lg hover:shadow-emerald-600"
-      >
-        <img
-          id="middle-image"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsynhvEh52iy3ZvsOqnOM625QiOe5KbP4V_yvLVbpSWIJs0UrL_DFBHtN91Xkho_r8hps&usqp=CAU"
-          alt=""
-          className="rounded-xl"
-        />
-      </button>
-
-      <div className="text-center text-2xl text-emerald-300">Javabogus</div>
-      {visible && (
-        <>
-          <img
-            className="absolute bottom-10 right-10 w-36"
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Duke_%28Java_mascot%29_waving.svg/1137px-Duke_%28Java_mascot%29_waving.svg.png"
-            alt=""
-          />
-          <img
-            className="absolute top-10 left-10 w-36"
-            src="https://i.kym-cdn.com/photos/images/original/002/018/144/1f8.png"
-            alt=""
-          />
-          <img
-            className="absolute top-10 right-10 w-48"
-            src="https://www.transparentpng.com/thumb/army-helicopter/pTKVr0-photo-military-nefer-union-png-army-helicopter.png"
-            alt=""
-          />
-          <img
-            className="absolute bottom-0 left-0 w-48"
-            src="http://thebookofbeing.net/mouse-deer.gif"
-            alt=""
-          />
-          <img
-            className="absolute bottom-0 left-1/2 w-48 -translate-x-1/2"
-            src="https://1.bp.blogspot.com/-9KuQVpfQBs4/YNXQPdskirI/AAAAAAAAYI8/3SV4I5uaMncFUFITpH2RkkgA_rmvZoK3wCLcBGAsYHQ/s500/fogueira.gif"
-            alt=""
-          />
-          <img
-            className="absolute top-0 left-1/2 w-48 -translate-x-1/2"
-            src="https://media.tenor.com/fSsxftCb8w0AAAAi/pikachu-running.gif"
-            alt=""
-          />
-        </>
-      )}
+    <div className="flex w-full flex-1 flex-col items-center justify-between gap-12 overflow-auto bg-zinc-900 p-8 md:justify-around">
+      <div className="flex flex-col gap-12">
+        <div className="mt-6 text-5xl text-emerald-100 md:text-center">
+          Bem-vindo a nossa loja de animas nem um pouco ilegais
+        </div>
+        <div className="text-lg text-emerald-100 md:text-center">
+          Nossa loja possui ama grande variedade de animais exóticos e
+          registrados*
+        </div>
+      </div>
+      <Link className="rounded-xl bg-emerald-400 p-4" to="/loja">
+        Compre Agora
+      </Link>
+      <div className="flex gap-12 max-md:flex-col">
+        {animals.slice(0, 3).map((animal) => (
+          <div className="max-w-full flex-1 rounded-lg bg-zinc-800 shadow-md md:max-w-xs">
+            <div className="p-3 text-center text-emerald-100">
+              {animal.name}
+            </div>
+            <img className="aspect-video" src={animal.image} alt="" />
+            <div className="p-3 text-center text-2xl text-emerald-300">
+              {animal.price}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
