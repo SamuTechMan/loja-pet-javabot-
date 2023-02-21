6efa2f4432f186854010ff0b19fd6b8a1b033ba3
João Rodolfo Reiter <joaorodolforeiter@protonmail.com>
2023-02-21 20:20:17 -0300

    fixed images on store

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,7 +2,7 @@ export default function ProductCard({ name, price, image }) {
   return (
     <div className="w-full max-w-lg rounded-xl bg-zinc-800 shadow-md">
       <div className="p-4 text-2xl font-semibold text-emerald-50">{name}</div>
-      <img className="aspect-[4/3]" src={image} alt="" />
+      <img className="aspect-[4/3] max-h-96" src={image} alt="" />
       <div className="flex items-center justify-between p-4 text-2xl text-emerald-50">
         <div>{price}</div>
         <button className="rounded-xl bg-zinc-700 p-2 font-semibold text-emerald-400">
