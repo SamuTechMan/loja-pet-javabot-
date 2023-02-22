172bf5e26440f37ac412d391d535e54357846c41
João Rodolfo Reiter <joaorodolforeiter@protonmail.com>
2023-02-21 21:46:22 -0300

    updated ProductInfo page

--- src/pages/ProductInfo.jsx
+++ src/pages/ProductInfo.jsx
@@ -5,17 +5,28 @@ export default function ProductInfo() {
   const { animal } = useParams();
   return (
     <div className="flex flex-1 items-center justify-center bg-zinc-900">
-      <div className="flex w-full items-center bg-zinc-800 p-10 shadow-md max-sm:flex-col sm:h-5/6 sm:w-5/6 sm:rounded-lg">
-        <div>
-          <img
-            className="aspect-square w-72"
-            src={animals[animal].image}
-            alt=""
-          />
-        </div>
-        <div>
-          <div>{animals[animal].name}</div>
-          <div>{animals[animal].price}</div>
+      <div className="flex h-full w-full gap-6 p-10 shadow-md max-md:flex-col max-md:text-center md:h-5/6 md:w-5/6 md:rounded-lg md:bg-zinc-800">
+        <img
+          className="aspect-square h-72 w-72 rounded-lg max-md:self-center"
+          src={animals[animal].image}
+          alt=""
+        />
+        <div className="flex flex-1 flex-col">
+          <div className="flex flex-1 flex-col gap-6">
+            <div className="text-5xl text-emerald-100">
+              {animals[animal].name}
+            </div>
+            <div className="text-emerald-300">{animals[animal].price}</div>
+            <div className="text-emerald-100">{animals[animal].info}</div>
+          </div>
+          <div className="flex w-full gap-3 text-lg">
+            <button className="w-full rounded-xl bg-zinc-700 p-4 text-emerald-100">
+              Adicionar ao Carrinho
+            </button>
+            <button className="w-full rounded-xl bg-emerald-400 p-4 text-zinc-900">
+              Comprar
+            </button>
+          </div>
         </div>
       </div>
     </div>
