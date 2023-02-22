5d3914825602a5eea912fe4086eb479d89d94192
SamuTechMan <samu1829almwida@gmail.com>
2023-02-21 21:10:14 -0300

    added info animals.

--- src/animals.js
+++ src/animals.js
@@ -4,190 +4,223 @@ export const animals = [
     price: "R$400",
     image:
       "https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/2212.600x450.webp?w=710&h=533",
+    info: "O axolote é um anfíbio que vive em ambientes escuros e de água doce. Além disso, este animal é considerado neotênico, ou seja, quando a espécie não troca de forma evolutiva durante seus ciclos de vida.",
   },
   {
     name: "Java Mouse Deer",
     price: "R$150.000",
     image:
       "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/cba54271-70f9-49df-bdb3-868f1bbbf7f8/db3dh1r-183548cc-1d38-4585-ae39-5a455ffc2669.jpg/v1/fill/w_1045,h_765,q_70,strp/java_mouse_deer_by_gredinia_db3dh1r-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTgxMyIsInBhdGgiOiJcL2ZcL2NiYTU0MjcxLTcwZjktNDlkZi1iZGIzLTg2OGYxYmJiZjdmOFwvZGIzZGgxci0xODM1NDhjYy0xZDM4LTQ1ODUtYWUzOS01YTQ1NWZmYzI2NjkuanBnIiwid2lkdGgiOiI8PTI0NzYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.CJH7BCODrcQn5B2ABuBVt4J9jxMWgd-DVXTl3Gc2LiE",
+    info: "O Java Mouse Deer (Tragulus javanicus) é uma espécie de ungulado da família Tragulidae. Quando atinge a maturidade, tem aproximadamente o tamanho de um coelho, tornando-o o menor ungulado vivo.",
   },
   {
     name: "Tamanduá dourado",
     price: "R$350.000",
     image:
       "https://www.portalmissal.com.br/images/blog/main/large/menor-tamandua-do-mundo-cabe-na-palma-da-mao-e-vive-no-brasil.jpg",
+    info: "O tamanduá-dourado vive geralmente sobre as árvores, descendo apenas para se alimentar e fazer suas necessidades. Essa espécie tem cerca de 77 cm e pesa 7 Kg.",
   },
   {
     name: "Aye-aye",
     price: "R$550.000",
     image:
       "https://meusanimais.com.br/wp-content/uploads/2021/04/animal-feio-assustado.jpg",
+    info: "O aie-aie, também ai-ai ou aye-aye é um primata estrepsirrino endémico de Madagáscar. É o único representante vivo da família Daubentoniidae.",  
   },
   {
     name: "Raposa Feneco",
     price: "R$650.000",
     image:
       "https://t2.ea.ltmcdn.com/pt/posts/3/5/3/o_feneco_como_animal_de_estimacao_20353_600.jpg",
+    info: "O feneco (Vulpes zerda)ou raposa-do-deserto, é uma pequena raposa crepuscular nativa do deserto do Saara e da Península do Sinai.", 
   },
   {
     name: "Maracanã-verdadeira",
     price: "R$700.000",
     image:
       "https://www.criadourovaleverde.com.br/wp-content/uploads/2021/06/3d5dc1_f851968cafcb47048d8bd9a9810b1818.jpg",
+    info: "A maracanã-verdadeira possui o nome científico de Primolius maracanã e é uma ave pertencente à família psittacidae e. Também é conhecida popularmente como arara-pequena, ararinha, Mulata-maracanã e papagaio-de-cara-branca.",  
   },
   {
     name: "Gavião-pato",
     price: "R$750.000",
     image:
       "https://s3.amazonaws.com/media.wikiaves.com.br/images/2422/2242348_e2f79b66a69582ae79378487a9d0eadd.jpg",
+    info: "O gavião-pato (nome científico: Spizaetus melanoleucus) é uma espécie de águia da família dos acipitrídeos (Accipitridae), pertencente ao género de águias nativas do neotrópicos Spizaetus.",
   },
   {
     name: "Vaca Medonha",
     price: "R$800.000",
     image:
       "https://preview.redd.it/ls6za4u0siu71.png?auto=webp&s=e366f2faf51c1866ed7dd3d2b6708b8f92011437",
+    info: "Vaca Medonha é uma figura misteriosa e desconhecida, que aparece sempre em momentos cruciais a fim de impedir que absurdos vão para o publico.", 
   },
   {
     name: "tucano-de-bico-verde",
     price: "R$900.000",
     image:
       "https://s3.amazonaws.com/media.wikiaves.com.br/images/6283/3826172_9443b4d686f5c7bfb46ad29b98ec9bf2.jpg",
+      info: "O tucano-de-bico-verde (Ramphastos dicolorus) é uma espécie de tucano nativa do Brasil, Bolívia, Argentina e Paraguai. Tais aves medem cerca de 48 centímetros de comprimento, possuindo, como o próprio nome popular indica, bico de cor verde, garganta e peito amarelos e barriga vermelha.",
   },
   {
     name: "Lutino Cara Branca",
     price: "R$1.000.000",
     image:
       "https://guiaanimal.net/uploads/content/image/8037/AdobeStock_208682110.jpeg",
+    info: "Essa fusão entre a calopsita cara branca com a arlequim lutino resulta em um jogo de eliminações: o gene da cara branca remove todo o amarelo e laranja que estariam presentes em um lutino e o gene lutino remove todo o cinza da cara branca. Ao final, teremos quase uma calopsita albina",
   },
   {
     name: "Gavião pega-macaco",
     price: "R$1.300.000",
     image:
       "https://www.passaro.org/wp-content/uploads/2018/11/Gavi%C3%A3o-Pega-Macaco-habitat-e1541095707159.jpg",
+    info: "O gavião-pega-macaco é uma ave caçadora do interior da floresta e a sua estratégia de caça é a captura das presas a grande velocidade, o que não permite que elas escapem. Alimenta-se de macacos (daí o seu nome popular), morcegos, esquilos, ratos etc",
   },
   {
     name: "Bico-de-pimenta",
     price: "R$1.500.000",
     image:
       "https://s3.amazonaws.com/media.wikiaves.com.br/images/1771/1771582_06fac92e858b14711ab4baeb539e82aa.jpg",
+    info: "Também conhecido como pimentão e bico-de-fogo, o bico-de-pimenta é uma ave passeriforme da família dos Thraupidae. Mede 22 cm de comprimento. Possui o corpo negro com bico vermelho-pimenta bem destacado que facilita sua identificação.",  
   },
   {
     name: "Tiê-sangue",
     price: "R$1.800.000",
     image:
       "https://s3.amazonaws.com/media.wikiaves.com.br/images/8481/1848831_3e05218466b5917ddf705753a530fc02.jpg",
+    info: "Quem viaja ao litoral brasileiro e se depara com um pontinho vermelho bem forte no meio da mata certamente pode estar diante de uma das aves mais espetaculares do mundo e um símbolo da Mata Atlântica. O tiê-sangue possui apenas 30 gramas e 19 centímetros, mas uma coloração marcante nos machos da espécie que inspirou seu nome popular.",  
   },
   {
     name: "jacaré branco",
     price: "R$2.200.000",
     image:
       "https://vegazeta.com.br/wp-content/uploads/2018/06/20d3d532eb7b4857dea12a9eefb9e1fa-1200x900.jpg",
+    info: "O Jacaré-Branco é considerado um dos menores crocodilianos do mundo, com tamanho máximo de 150 cm de comprimento. Tem distribuição em dez países da América do Sul, e no Brasil pode ser encontrado nos biomas Amazônia, Caatinga, Cerrado, Mata Atlântica e no entorno do Pantanal.",
   },
   {
     name: "Macaco-narigudo",
     price: "R$5.000.000",
-    image: "https://orionneto.files.wordpress.com/2013/02/animal_09.jpg?w=980",
+    image: 
+        "https://orionneto.files.wordpress.com/2013/02/animal_09.jpg?w=980",
+    info: "O macaco-narigudo (Nasalis larvatus) é um macaco da família dos cercopitecídeos, endêmico das florestas tropicais de Bornéu, o qual se encontra localizado no oceano Índico. Animais de pelagem acastanhada, os quais chamam atenção pelo tamanho do seu nariz.",
   },
   {
     name: "Lula-verme",
     price: "R$6.500.000",
     image:
       "https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/27022.webp?w=710&h=454",
+    info: " O verme-lula é um anelídeo tão peculiar que, após sua recente descoberta, é considerado a única espécie de seu gênero. O verme-lula (Teuthidodrilus samae) é uma espécie curiosa pertencente à família dos anelídeos.",
   },
   {
     name: "Lesma ninja de Bornéu",
     price: "R$8.000.000",
     image:
       "https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/27185.jpg?w=1600",
+    info: "Características da lesma ninja de Bornéu Seu nome científico é Ibycus Rachelae e é uma espécie de molusco da classe dos gastrópodes com uma cor verde e amarela muito particular. Foi descoberto nas florestas montanhosas da Malásia, especificamente na selva Gunung Kinabalu a quase 2.000 metros acima do nível do mar.",
   },
   {
     name: "Macaco de Mianmar",
     price: "R$9.000.000",
     image:
       "https://hypescience.com/wp-content/uploads/2010/12/anibiz5.jpg",
+    info: "Uma nova espécie de macaco com o nariz arrebitado foi encontrada no nordeste de Mianmar (antiga Birmânia), na Ásia. As narinas do animal são voltadas para cima, portanto, quando chove, a água entra pelas cavidades, fazendo com que o macaco espirre.",
   },
   {
     name: "Rato-toupeira-pelado",
     price: "R$10.000.000",
     image:
       "https://igec.com.br/wp-content/uploads/2021/04/74f8567605bf2b58211ea4e7bd2b74b8_Rato-toupeira-pelado.jpg",
+    info:"Conheça o rato-toupeira-pelado, o roedor africano que não envelhece Animal desafia as leis da biologia, mas não por sua beleza 2 min de leitura.", 
   },
   {
     name: "Jerboar",
     price: "R$12.000.000",
     image:
       "https://igec.com.br/wp-content/uploads/2021/04/705980d1af4ffcd24f9c029bd29c4610_Jerboa.jpg",
+    info: "O tipo de movimento é um ótimo jeito de evitar o contato com as areias da região onde vivem, que são muito quentes. Com uma média de altura de menos de 10 centímetros, esses animais podem pular a até 1,8 m de altura.",  
   },
   {
     name: "Tenreque-raiado-das-planícies",
     price: "R$13.000.000",
     image:
       "https://igec.com.br/wp-content/uploads/2021/04/c4dadbe3b9478727e79cd9eb43b36f1e_tenreque-raiado-das-planicies.jpg",
+    info: "O tenreque-raiado-das-planícies (Hemicentetes semispinosus) é uma espécie de mamífero da família Tenrecidae, endêmica de Madagáscar. É o único mamífero capaz de estridular.",
   },
   {
     name: "Colugo",
     price: "R$13.500.000",
     image:
       "https://igec.com.br/wp-content/uploads/2021/04/32508d44377bb608885ce78dd0a6d52c_ngs0_8091-2.jpg",
+    info: "Anote este nome: colugo. Você muito provavelmente não tem a mínima idéia do que se trata, então aí vai um resumo: pequeno, felpudo, com uma membrana gigantesca de pele entre os membros que lhe permite planar.",
   },
   {
     name: "Ocapi",
     price: "R$13.700.000",
     image:
       "https://igec.com.br/wp-content/uploads/2021/04/3f6b04365f20b098bc5993429f3c8d46_ocapi_capa_animal_widexl.jpg",
+    info: "o ocapi é um mamífero localizado somente na República Democrática do Congo, na África. Nesse sentido, essa espécie foi descoberta somente por volta de 1900 e apresenta forte parentesco com as girafas.", 
   },
   {
     name: "Babirusa",
     price: "R$14.000.000",
     image:
       "https://igec.com.br/wp-content/uploads/2021/04/7e8b05d476fd5d7cfd1df14b48045339_Babirusa.jpg",
+    info: "Os Babirusas, cujo nome se traduz diretamente em Pig Deer em malaio malaio e inglês, são um gênero, Babyrousa, da família suína encontrada nas ilhas indonésias de Sulawesi, Togian, Sula e Buru.",  
   },
   {
     name: "Peixe-Gota",
     price: "R$14.800.000",
     image:
       "https://igec.com.br/wp-content/uploads/2021/04/41d9810e4f466644cb104b2495f85972_Peixe-Gota.jpg",
+    info: "O peixe-bolha é um animal considerado estranho para muitas pessoas e está na lista dos mais exóticos do mundo. Por sua estética não muito convencional, é chamado de feio e esquisito pelos leigos.",
   },
   {
     name: "Peixe-mão rosa",
     price: "R$15.000.000",
     image:
       "https://ogimg.infoglobo.com.br/in/25331649-7eb-3d6/FT1086A/_122493152_pink_handfish_by_karen_gowle.2e16d0ba.fill-1200x600-c100.jpg",
+    info: "O peixe mão de rosa (Brachiopsilus dianthus) é uma espécie de peixe que tem nadadeiras em forma de mão que ele usa para se mover ao longo do fundo do mar. Características. É pequeno em tamanho, medindo cerca de 10 cm. Seu corpo é de cor rosa claro, com tons mais escuros na parte superior.",  
   },
   {
     name: "O engolidor-negro",
     price: "R$17.000.000",
     image:
       "https://t1.ea.ltmcdn.com/pt/posts/7/9/3/1_o_engolidor_negro_21397_0_600.webp",
+    info: " O engolidor negro é uma espécie de peixe da ordem Perciformes e da família Chiasmodontidae. Ele habita as profundezas escuras do mar e seu nome científico é Chiasmodon niger.",   
   },
   {
-    name: " peixe-comedor-de-língua",
+    name: "peixe-comedor-de-língua",
     price: "R$20.000.000",
     image:
       "https://img.r7.com/images/parasita-lingua-peixe-terror-redes-sociais-09082022183140832?dimensions=771x420&no_crop=true",
+    info: "peixe-comedor-de-língua, é um crustáceo parasita da família Cymothoidae. Esse parasita entra no corpo de peixes pelas brânquias e então se agarra à base da língua do animal. A fêmea se agarra à língua e o macho às guelras, logo atrás da fêmea.",  
   },
   {
     name: "Stargazer do norte",
     price: "R$22.000.000",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Northern_Stargazer.JPG/1200px-Northern_Stargazer.JPG",
+    info: "O Stargazer do norte é um peixe marrom-escuro com manchas brancas que gradualmente ficam maiores da cabeça à cauda. Os adultos têm: * Boca e olhos voltados para cima.",  
   },
   {
     name: "Tubarão tapete",
     price: "R$24.000.000",
     image:
       "https://t2.ea.ltmcdn.com/pt/posts/7/9/3/4_tubarao_tapete_21397_3_600.webp",
+    info: "O Tubarão Tapete (Eucrossorhinus dasypogon) é um tubarão pouco conhecido encontrados na plataforma continental e recifes. Provavelmente se alimenta de peixes de fundo e invertebrados, também conhecidos por comer peixes teleósteos noturna como squirrelfish e soldierfish",  
   },
   {
     name: "Tubarão-cobra",
     price: "R$28.000.000",
     image:
       "https://t1.ea.ltmcdn.com/pt/posts/7/9/3/5_tubarao_cobra_21397_4_600.webp",
+    info: "A pessoa comum será capaz de ver um tubarão-cobra apenas em fotos. ... Como predadores, a principal é regular cardumes, consequentemente, não atacam qualquer peixe, mas, os debilitados que apresentam doenças ou os mais fracos. Em resumo, tubarões alimentam-se de peixes e invertebrados.",  
   },
   {
     name: "arraia manta gigante",
     price: "R$900.000.000",
     image:
       "https://marsemfim.com.br/wp-content/uploads/2016/01/a-jamanta-calamazista.jpg",
+    info: "Conhecidas como gigantes gentis, as raias mantas compõe a chamada megafauna carismática, animais que valem muito mais vivos do que mortos, movimentando o turismo de mergulho em todo o mundo na qualidade de recurso perene do rico segmento do turismo mundial.",  
   },
 ];
