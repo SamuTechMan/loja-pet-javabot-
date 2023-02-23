7dbbd72dd413c512eb34fb701df5ed7cb32a41cb
João Rodolfo Reiter <joaorodolforeiter@protonmail.com>
2023-02-23 17:23:33 +0000

    updated homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { animals } from "../animals";
 
 export default function Home() {
   return (
-    <div className="flex w-full flex-1 flex-col items-center justify-between gap-12 overflow-auto bg-zinc-900 p-8 md:justify-around">
+    <div className="flex w-full flex-1 flex-col items-center justify-between gap-12 overflow-auto bg-zinc-900 p-6 md:justify-around">
       <div className="flex flex-col gap-12">
         <div className="mt-6 text-5xl text-emerald-100 md:text-center">
           Bem-vindo a nossa loja de animas nem um pouco ilegais
@@ -13,10 +13,13 @@ export default function Home() {
           registrados*
         </div>
       </div>
-      <Link className="rounded-xl bg-emerald-400 p-4" to="/loja">
+      <Link
+        className="rounded-xl bg-emerald-400 p-4 text-center max-md:w-full"
+        to="/loja"
+      >
         Compre Agora
       </Link>
-      <div className="flex gap-12 max-md:flex-col">
+      <div className="flex gap-6 max-md:hidden max-md:flex-col">
         {animals.slice(0, 3).map((animal) => (
           <div className="max-w-full flex-1 rounded-lg bg-zinc-800 shadow-md md:max-w-xs">
             <div className="p-3 text-center text-emerald-100">
