a3862c32bbcab5550e5b39fcdd96fe6f37fa5ff9
João Rodolfo Reiter <joaorodolforeiter@protonmail.com>
2023-02-23 17:16:58 +0000

    added loja label to store page

--- src/pages/Loja.jsx
+++ src/pages/Loja.jsx
@@ -3,8 +3,9 @@ import { animals } from "../animals";
 
 export default function Loja() {
   return (
-    <div className="flex flex-col items-center overflow-auto bg-zinc-900">
-      <div className="grid flex-1 items-center gap-12 p-10 lg:grid-cols-2 xl:grid-cols-3">
+    <div className="flex flex-col items-center gap-6 overflow-auto bg-zinc-900 p-6">
+      <div className="self-start text-4xl text-emerald-100">Loja</div>
+      <div className="grid flex-1 items-center gap-6 lg:grid-cols-2 xl:grid-cols-3">
         {animals.map((animal, i) => (
           <ProductCard
             key={i}
