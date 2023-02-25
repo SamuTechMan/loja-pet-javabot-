9faff5d53bb39bb4cc42c93778fb25679c9bf411
João Rodolfo Reiter <joaorodolforeiter@protonmail.com>
2023-02-24 21:06:12 -0300

    added id to the animals

--- src/animals.js
+++ src/animals.js
@@ -1,7 +1,8 @@
-export const animalTypes = ["land","hybrid","fly","swim","dead"]
+export const animalTypes = ["land", "hybrid", "fly", "swim", "dead"];
 
 export const animals = [
   {
+    id: "ntOZMGQRdT",
     name: "Axolote mexicano",
     categoria: "hybrid",
     price: 400,
@@ -10,6 +11,7 @@ export const animals = [
     info: "O axolote é um anfíbio que vive em ambientes escuros e de água doce. Além disso, este animal é considerado neotênico, ou seja, quando a espécie não troca de forma evolutiva durante seus ciclos de vida.",
   },
   {
+    id: "rbrH4Mp9n0",
     name: "Java Mouse Deer",
     categoria: "land",
     price: 150_000,
@@ -18,6 +20,7 @@ export const animals = [
     info: "O Java Mouse Deer (Tragulus javanicus) é uma espécie de ungulado da família Tragulidae. Quando atinge a maturidade, tem aproximadamente o tamanho de um coelho, tornando-o o menor ungulado vivo.",
   },
   {
+    id: "x-8he8rwxU",
     name: "Tamanduá dourado",
     categoria: "land",
     price: 350_000,
@@ -26,30 +29,34 @@ export const animals = [
     info: "O tamanduá-dourado vive geralmente sobre as árvores, descendo apenas para se alimentar e fazer suas necessidades. Essa espécie tem cerca de 77 cm e pesa 7 Kg.",
   },
   {
+    id: "S8UH4eHWh8",
     name: "Aye-aye",
     categoria: "land",
     price: 550_000,
     image:
       "https://meusanimais.com.br/wp-content/uploads/2021/04/animal-feio-assustado.jpg",
-    info: "O aie-aie, também ai-ai ou aye-aye é um primata estrepsirrino endémico de Madagáscar. É o único representante vivo da família Daubentoniidae.",  
+    info: "O aie-aie, também ai-ai ou aye-aye é um primata estrepsirrino endémico de Madagáscar. É o único representante vivo da família Daubentoniidae.",
   },
   {
+    id: "6DyHGqiQ7R",
     name: "Raposa Feneco",
     categoria: "land",
     price: 650_000,
     image:
       "https://t2.ea.ltmcdn.com/pt/posts/3/5/3/o_feneco_como_animal_de_estimacao_20353_600.jpg",
-    info: "O feneco (Vulpes zerda)ou raposa-do-deserto, é uma pequena raposa crepuscular nativa do deserto do Saara e da Península do Sinai.", 
+    info: "O feneco (Vulpes zerda)ou raposa-do-deserto, é uma pequena raposa crepuscular nativa do deserto do Saara e da Península do Sinai.",
   },
   {
+    id: "cK3OFZQLfx",
     name: "Maracanã-verdadeira",
     categoria: "fly",
     price: 700_000,
     image:
       "https://www.criadourovaleverde.com.br/wp-content/uploads/2021/06/3d5dc1_f851968cafcb47048d8bd9a9810b1818.jpg",
-    info: "A maracanã-verdadeira possui o nome científico de Primolius maracanã e é uma ave pertencente à família psittacidae e. Também é conhecida popularmente como arara-pequena, ararinha, Mulata-maracanã e papagaio-de-cara-branca.",  
+    info: "A maracanã-verdadeira possui o nome científico de Primolius maracanã e é uma ave pertencente à família psittacidae e. Também é conhecida popularmente como arara-pequena, ararinha, Mulata-maracanã e papagaio-de-cara-branca.",
   },
   {
+    id: "WSP0TkL2qT",
     name: "Gavião-pato",
     categoria: "fly",
     price: 750_000,
@@ -58,22 +65,25 @@ export const animals = [
     info: "O gavião-pato (nome científico: Spizaetus melanoleucus) é uma espécie de águia da família dos acipitrídeos (Accipitridae), pertencente ao género de águias nativas do neotrópicos Spizaetus.",
   },
   {
+    id: "Grvx173xtq",
     name: "Vaca Medonha",
     categoria: "land",
     price: 800_000,
     image:
       "https://preview.redd.it/ls6za4u0siu71.png?auto=webp&s=e366f2faf51c1866ed7dd3d2b6708b8f92011437",
-    info: "Vaca Medonha é uma figura misteriosa e desconhecida, que aparece sempre em momentos cruciais a fim de impedir que absurdos vão para o publico.", 
+    info: "Vaca Medonha é uma figura misteriosa e desconhecida, que aparece sempre em momentos cruciais a fim de impedir que absurdos vão para o publico.",
   },
   {
+    id: "Zl4s7Pc48y",
     name: "tucano-de-bico-verde",
     categoria: "fly",
     price: 900_000,
     image:
       "https://s3.amazonaws.com/media.wikiaves.com.br/images/6283/3826172_9443b4d686f5c7bfb46ad29b98ec9bf2.jpg",
-      info: "O tucano-de-bico-verde (Ramphastos dicolorus) é uma espécie de tucano nativa do Brasil, Bolívia, Argentina e Paraguai. Tais aves medem cerca de 48 centímetros de comprimento, possuindo, como o próprio nome popular indica, bico de cor verde, garganta e peito amarelos e barriga vermelha.",
+    info: "O tucano-de-bico-verde (Ramphastos dicolorus) é uma espécie de tucano nativa do Brasil, Bolívia, Argentina e Paraguai. Tais aves medem cerca de 48 centímetros de comprimento, possuindo, como o próprio nome popular indica, bico de cor verde, garganta e peito amarelos e barriga vermelha.",
   },
   {
+    id: "lDdhrDppUk",
     name: "Lutino Cara Branca",
     categoria: "fly",
     price: 1_000_000,
@@ -82,6 +92,7 @@ export const animals = [
     info: "Essa fusão entre a calopsita cara branca com a arlequim lutino resulta em um jogo de eliminações: o gene da cara branca remove todo o amarelo e laranja que estariam presentes em um lutino e o gene lutino remove todo o cinza da cara branca. Ao final, teremos quase uma calopsita albina",
   },
   {
+    id: "Tp7fXzvL7B",
     name: "Gavião pega-macaco",
     categoria: "fly",
     price: 1_300_000,
@@ -90,22 +101,25 @@ export const animals = [
     info: "O gavião-pega-macaco é uma ave caçadora do interior da floresta e a sua estratégia de caça é a captura das presas a grande velocidade, o que não permite que elas escapem. Alimenta-se de macacos (daí o seu nome popular), morcegos, esquilos, ratos etc",
   },
   {
+    id: "oF0mvmGaVu",
     name: "Bico-de-pimenta",
     categoria: "fly",
     price: 1_500_000,
     image:
       "https://s3.amazonaws.com/media.wikiaves.com.br/images/1771/1771582_06fac92e858b14711ab4baeb539e82aa.jpg",
-    info: "Também conhecido como pimentão e bico-de-fogo, o bico-de-pimenta é uma ave passeriforme da família dos Thraupidae. Mede 22 cm de comprimento. Possui o corpo negro com bico vermelho-pimenta bem destacado que facilita sua identificação.",  
+    info: "Também conhecido como pimentão e bico-de-fogo, o bico-de-pimenta é uma ave passeriforme da família dos Thraupidae. Mede 22 cm de comprimento. Possui o corpo negro com bico vermelho-pimenta bem destacado que facilita sua identificação.",
   },
   {
+    id: "CydzHTJLsm",
     name: "Tiê-sangue",
     categoria: "fly",
     price: 1_800_000,
     image:
       "https://s3.amazonaws.com/media.wikiaves.com.br/images/8481/1848831_3e05218466b5917ddf705753a530fc02.jpg",
-    info: "Quem viaja ao litoral brasileiro e se depara com um pontinho vermelho bem forte no meio da mata certamente pode estar diante de uma das aves mais espetaculares do mundo e um símbolo da Mata Atlântica. O tiê-sangue possui apenas 30 gramas e 19 centímetros, mas uma coloração marcante nos machos da espécie que inspirou seu nome popular.",  
+    info: "Quem viaja ao litoral brasileiro e se depara com um pontinho vermelho bem forte no meio da mata certamente pode estar diante de uma das aves mais espetaculares do mundo e um símbolo da Mata Atlântica. O tiê-sangue possui apenas 30 gramas e 19 centímetros, mas uma coloração marcante nos machos da espécie que inspirou seu nome popular.",
   },
   {
+    id: "hHeHWlneaD",
     name: "jacaré branco",
     categoria: "hybrid",
     price: 2_200_000,
@@ -114,14 +128,15 @@ export const animals = [
     info: "O Jacaré-Branco é considerado um dos menores crocodilianos do mundo, com tamanho máximo de 150 cm de comprimento. Tem distribuição em dez países da América do Sul, e no Brasil pode ser encontrado nos biomas Amazônia, Caatinga, Cerrado, Mata Atlântica e no entorno do Pantanal.",
   },
   {
+    id: "Mwr7oLTPLx",
     name: "Macaco-narigudo",
     categoria: "land",
     price: 5_000_000,
-    image: 
-        "https://orionneto.files.wordpress.com/2013/02/animal_09.jpg?w=980",
+    image: "https://orionneto.files.wordpress.com/2013/02/animal_09.jpg?w=980",
     info: "O macaco-narigudo (Nasalis larvatus) é um macaco da família dos cercopitecídeos, endêmico das florestas tropicais de Bornéu, o qual se encontra localizado no oceano Índico. Animais de pelagem acastanhada, os quais chamam atenção pelo tamanho do seu nariz.",
   },
   {
+    id: "JvCAw8DeCe",
     name: "Lula-verme",
     categoria: "swim",
     price: 6_500_000,
@@ -130,6 +145,7 @@ export const animals = [
     info: " O verme-lula é um anelídeo tão peculiar que, após sua recente descoberta, é considerado a única espécie de seu gênero. O verme-lula (Teuthidodrilus samae) é uma espécie curiosa pertencente à família dos anelídeos.",
   },
   {
+    id: "qNmWNzX9eW",
     name: "Lesma ninja de Bornéu",
     categoria: "fly",
     price: 8_000_000,
@@ -138,30 +154,33 @@ export const animals = [
     info: "Características da lesma ninja de Bornéu Seu nome científico é Ibycus Rachelae e é uma espécie de molusco da classe dos gastrópodes com uma cor verde e amarela muito particular. Foi descoberto nas florestas montanhosas da Malásia, especificamente na selva Gunung Kinabalu a quase 2.000 metros acima do nível do mar.",
   },
   {
+    id: "7QOdgxyuwI",
     name: "Macaco de Mianmar",
     categoria: "land",
     price: 9_000_000,
-    image:
-      "https://hypescience.com/wp-content/uploads/2010/12/anibiz5.jpg",
+    image: "https://hypescience.com/wp-content/uploads/2010/12/anibiz5.jpg",
     info: "Uma nova espécie de macaco com o nariz arrebitado foi encontrada no nordeste de Mianmar (antiga Birmânia), na Ásia. As narinas do animal são voltadas para cima, portanto, quando chove, a água entra pelas cavidades, fazendo com que o macaco espirre.",
   },
   {
+    id: "0Wt0y7Mjy2",
     name: "Rato-toupeira-pelado",
     categoria: "land",
     price: 10_000_000,
     image:
       "https://igec.com.br/wp-content/uploads/2021/04/74f8567605bf2b58211ea4e7bd2b74b8_Rato-toupeira-pelado.jpg",
-    info:"Conheça o rato-toupeira-pelado, o roedor africano que não envelhece Animal desafia as leis da biologia, mas não por sua beleza 2 min de leitura.", 
+    info: "Conheça o rato-toupeira-pelado, o roedor africano que não envelhece Animal desafia as leis da biologia, mas não por sua beleza 2 min de leitura.",
   },
   {
+    id: "GFppbGyIx6",
     name: "Jerboar",
     categoria: "land",
     price: 12_000_000,
     image:
       "https://igec.com.br/wp-content/uploads/2021/04/705980d1af4ffcd24f9c029bd29c4610_Jerboa.jpg",
-    info: "O tipo de movimento é um ótimo jeito de evitar o contato com as areias da região onde vivem, que são muito quentes. Com uma média de altura de menos de 10 centímetros, esses animais podem pular a até 1,8 m de altura.",  
+    info: "O tipo de movimento é um ótimo jeito de evitar o contato com as areias da região onde vivem, que são muito quentes. Com uma média de altura de menos de 10 centímetros, esses animais podem pular a até 1,8 m de altura.",
   },
   {
+    id: "tT8DMfSz7N",
     name: "Tenreque-raiado-das-planícies",
     categoria: "land",
     price: 13_000_000,
@@ -170,6 +189,7 @@ export const animals = [
     info: "O tenreque-raiado-das-planícies (Hemicentetes semispinosus) é uma espécie de mamífero da família Tenrecidae, endêmica de Madagáscar. É o único mamífero capaz de estridular.",
   },
   {
+    id: "XdZuEgcgkO",
     name: "Colugo",
     categoria: "hybrid",
     price: 13_500_000,
@@ -178,22 +198,25 @@ export const animals = [
     info: "Anote este nome: colugo. Você muito provavelmente não tem a mínima idéia do que se trata, então aí vai um resumo: pequeno, felpudo, com uma membrana gigantesca de pele entre os membros que lhe permite planar.",
   },
   {
+    id: "TUToCspxCj",
     name: "Ocapi",
     categoria: "land",
     price: 13_700_000,
     image:
       "https://igec.com.br/wp-content/uploads/2021/04/3f6b04365f20b098bc5993429f3c8d46_ocapi_capa_animal_widexl.jpg",
-    info: "o ocapi é um mamífero localizado somente na República Democrática do Congo, na África. Nesse sentido, essa espécie foi descoberta somente por volta de 1900 e apresenta forte parentesco com as girafas.", 
+    info: "o ocapi é um mamífero localizado somente na República Democrática do Congo, na África. Nesse sentido, essa espécie foi descoberta somente por volta de 1900 e apresenta forte parentesco com as girafas.",
   },
   {
+    id: "LvGyy14tC9",
     name: "Babirusa",
     categoria: "land",
     price: 14_000_000,
     image:
       "https://igec.com.br/wp-content/uploads/2021/04/7e8b05d476fd5d7cfd1df14b48045339_Babirusa.jpg",
-    info: "Os Babirusas, cujo nome se traduz diretamente em Pig Deer em malaio malaio e inglês, são um gênero, Babyrousa, da família suína encontrada nas ilhas indonésias de Sulawesi, Togian, Sula e Buru.",  
+    info: "Os Babirusas, cujo nome se traduz diretamente em Pig Deer em malaio malaio e inglês, são um gênero, Babyrousa, da família suína encontrada nas ilhas indonésias de Sulawesi, Togian, Sula e Buru.",
   },
   {
+    id: "UYPDf3cDGb",
     name: "Peixe-Gota",
     categoria: "swim",
     price: 14_800_000,
@@ -202,66 +225,74 @@ export const animals = [
     info: "O peixe-bolha é um animal considerado estranho para muitas pessoas e está na lista dos mais exóticos do mundo. Por sua estética não muito convencional, é chamado de feio e esquisito pelos leigos.",
   },
   {
+    id: "7rI6v9PaEM",
     name: "Peixe-mão rosa",
     categoria: "swim",
     price: 15_000_000,
     image:
       "https://ogimg.infoglobo.com.br/in/25331649-7eb-3d6/FT1086A/_122493152_pink_handfish_by_karen_gowle.2e16d0ba.fill-1200x600-c100.jpg",
-    info: "O peixe mão de rosa (Brachiopsilus dianthus) é uma espécie de peixe que tem nadadeiras em forma de mão que ele usa para se mover ao longo do fundo do mar. Características. É pequeno em tamanho, medindo cerca de 10 cm. Seu corpo é de cor rosa claro, com tons mais escuros na parte superior.",  
+    info: "O peixe mão de rosa (Brachiopsilus dianthus) é uma espécie de peixe que tem nadadeiras em forma de mão que ele usa para se mover ao longo do fundo do mar. Características. É pequeno em tamanho, medindo cerca de 10 cm. Seu corpo é de cor rosa claro, com tons mais escuros na parte superior.",
   },
   {
+    id: "ywJY3FjoeI",
     name: "O engolidor-negro",
     categoria: "dead",
     price: 17_000_000,
     image:
       "https://t1.ea.ltmcdn.com/pt/posts/7/9/3/1_o_engolidor_negro_21397_0_600.webp",
-    info: " O engolidor negro é uma espécie de peixe da ordem Perciformes e da família Chiasmodontidae. Ele habita as profundezas escuras do mar e seu nome científico é Chiasmodon niger.",   
+    info: " O engolidor negro é uma espécie de peixe da ordem Perciformes e da família Chiasmodontidae. Ele habita as profundezas escuras do mar e seu nome científico é Chiasmodon niger.",
   },
   {
+    id: "nr6N1q0sn4",
     name: "peixe-comedor-de-língua",
     categoria: "swim",
     price: 20_000_000,
     image:
       "https://img.r7.com/images/parasita-lingua-peixe-terror-redes-sociais-09082022183140832?dimensions=771x420&no_crop=true",
-    info: "peixe-comedor-de-língua, é um crustáceo parasita da família Cymothoidae. Esse parasita entra no corpo de peixes pelas brânquias e então se agarra à base da língua do animal. A fêmea se agarra à língua e o macho às guelras, logo atrás da fêmea.",  
+    info: "peixe-comedor-de-língua, é um crustáceo parasita da família Cymothoidae. Esse parasita entra no corpo de peixes pelas brânquias e então se agarra à base da língua do animal. A fêmea se agarra à língua e o macho às guelras, logo atrás da fêmea.",
   },
   {
+    id: "NL7jJcU4YU",
     name: "Stargazer do norte",
     categoria: "swim",
     price: 22_000_000,
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1uSQ1W3KtYeFtft7ofdegE42RpZH0oSCPN_93OwKKXhIIQVSRRwkLQQFYTQ2uH2NnPtE&usqp=CAU",
-    info: "O Stargazer do norte é um peixe marrom-escuro com manchas brancas que gradualmente ficam maiores da cabeça à cauda. Os adultos têm: * Boca e olhos voltados para cima.",  
+    info: "O Stargazer do norte é um peixe marrom-escuro com manchas brancas que gradualmente ficam maiores da cabeça à cauda. Os adultos têm: * Boca e olhos voltados para cima.",
   },
   {
+    id: "slf5kQ9R9T",
     name: "Tubarão tapete",
     categoria: "swim",
     price: 24_000_000,
     image:
       "https://t2.ea.ltmcdn.com/pt/posts/7/9/3/4_tubarao_tapete_21397_3_600.webp",
-    info: "O Tubarão Tapete (Eucrossorhinus dasypogon) é um tubarão pouco conhecido encontrados na plataforma continental e recifes. Provavelmente se alimenta de peixes de fundo e invertebrados, também conhecidos por comer peixes teleósteos noturna como squirrelfish e soldierfish",  
+    info: "O Tubarão Tapete (Eucrossorhinus dasypogon) é um tubarão pouco conhecido encontrados na plataforma continental e recifes. Provavelmente se alimenta de peixes de fundo e invertebrados, também conhecidos por comer peixes teleósteos noturna como squirrelfish e soldierfish",
   },
   {
+    id: "FoVqGWWbW1",
     name: "Tubarão-cobra",
     categoria: "swim",
     price: 28_000_000,
     image:
       "https://t1.ea.ltmcdn.com/pt/posts/7/9/3/5_tubarao_cobra_21397_4_600.webp",
-    info: "A pessoa comum será capaz de ver um tubarão-cobra apenas em fotos. ... Como predadores, a principal é regular cardumes, consequentemente, não atacam qualquer peixe, mas, os debilitados que apresentam doenças ou os mais fracos. Em resumo, tubarões alimentam-se de peixes e invertebrados.",  
+    info: "A pessoa comum será capaz de ver um tubarão-cobra apenas em fotos. ... Como predadores, a principal é regular cardumes, consequentemente, não atacam qualquer peixe, mas, os debilitados que apresentam doenças ou os mais fracos. Em resumo, tubarões alimentam-se de peixes e invertebrados.",
   },
   {
+    id: "1AMEqqxAnD",
     name: "arraia manta gigante",
     categoria: "dead",
     price: 900_000_000,
     image:
       "https://marsemfim.com.br/wp-content/uploads/2016/01/a-jamanta-calamazista.jpg",
-    info: "Conhecidas como gigantes gentis, as raias mantas compõe a chamada megafauna carismática, animais que valem muito mais vivos do que mortos, movimentando o turismo de mergulho em todo o mundo na qualidade de recurso perene do rico segmento do turismo mundial.",  
+    info: "Conhecidas como gigantes gentis, as raias mantas compõe a chamada megafauna carismática, animais que valem muito mais vivos do que mortos, movimentando o turismo de mergulho em todo o mundo na qualidade de recurso perene do rico segmento do turismo mundial.",
   },
   {
+    id: "zbuxLQVcZM",
     name: "Tubarão Duende",
     categoria: "Duende",
     price: 25_555_555,
-    image:"https://imagens.mdig.com.br/bichos/Mitsukurina_owstoni_01.jpg",
-    info: "Esqueça os tubarões devoradores de homens de 'Sharknado', ou ainda o grande tubarão-branco de 'Tubarão' ou ainda o megalodonte de 'Megatubarão': há um novo tubarão superstar na cidade e ele lhe dará mais pesadelos do que os três juntos. Conheça o tubarão-duende, também conhecido como tubarão-goblin (Mitsukurina owstoni). O peixe misterioso e esquivo foi encontrado pela primeira vez na costa leste do Japão, onde foi descrito em 1898. Com seu focinho incomumente longo e tom de pele carnuda, o tubarão recebeu o nome de tenguzame."
-  }
+    image: "https://imagens.mdig.com.br/bichos/Mitsukurina_owstoni_01.jpg",
+    info: "Esqueça os tubarões devoradores de homens de 'Sharknado', ou ainda o grande tubarão-branco de 'Tubarão' ou ainda o megalodonte de 'Megatubarão': há um novo tubarão superstar na cidade e ele lhe dará mais pesadelos do que os três juntos. Conheça o tubarão-duende, também conhecido como tubarão-goblin (Mitsukurina owstoni). O peixe misterioso e esquivo foi encontrado pela primeira vez na costa leste do Japão, onde foi descrito em 1898. Com seu focinho incomumente longo e tom de pele carnuda, o tubarão recebeu o nome de tenguzame.",
+  },
 ];
